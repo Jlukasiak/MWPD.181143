227afec21a4e8c732664f2f0a59b4c7548a9707a
Jakub Łukasiak <184928295+Jlukasiak@users.noreply.github.com>
2026-03-15 17:51:22 +0100

    Add files via upload

---
+++ Lab02_wpd/mystats.py
@@ -0,0 +1,43 @@
+def median(data):
+
+
+    if not data:
+        return None
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+    mid = n // 2
+
+    if n % 2 == 0:
+       return (sorted_data[mid - 1] + sorted_data[mid]) / 2
+    else:
+        return sorted_data[mid]
+
+
+def trimmed_mean(data, p):
+
+    if not data or not (0 <= p < 0.5):
+        return None
+
+
+    sorted_data = sorted(data)
+    n = len(sorted_data)
+
+    k = int(n * p)
+
+    trimmed_data = sorted_data[k: n - k]
+
+    if not trimmed_data:
+        return None
+    return sum(trimmed_data) / len(trimmed_data)
+
+
+def main():
+
+    dane = [1, 2, 3, 4, 5, 100]
+
+    print(f"Mediana: {median(dane)}")
+    print(f"Średnia ucinana (p=0.1): {trimmed_mean(dane, 0.1)}")
+
+
+if __name__ == '__main__':
+    main()
